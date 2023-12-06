2e1ead087f2cd98ddb654f2af7187b5886594ba0
VitthalGund <vitthalgund30@gmail.com>
2023-12-06 17:27:39 +0530

    about section updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,12 +22,7 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                    Welcome to MovieMagic Hub, your go-to destination for discovering the best in movies and TV shows! At MovieMagic Hub, we believe that entertainment should be an enriching experience, and we're dedicated to making your journey through the world of cinema a delightful one.
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
